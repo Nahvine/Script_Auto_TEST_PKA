d6f1568fec20cd1a5be2e9fe047bcf622e9bd9f3
Yurayuraa <101639160+Nahvine@users.noreply.github.com>
2026-01-29 10:33:13 +0700

    Change default API keys in getKeys function
    Updated default API keys in local storage initialization.

--- uiz-auto-gemini.user.js
+++ uiz-auto-gemini.user.js
@@ -125,7 +125,9 @@
     let raw = localStorage.getItem(LS_KEYS);
     if(!raw){
       const defaults=[
-        "AIzaSyA6P-Ec1aKvBVyUqgZByb515IL_2xOvDqM"
+        "AIzaSyD7VvHT5S-yntEDVL82wgOSsPmYSWuaXs8",
+        "AIzaSyBQJfAFkGRsv_jhL0FP1Sf4eXVfNhoo7Ec",
+        "AIzaSyC5el-uH5Ca6DUbVilo008nahv2pwn3tPw"
       ];
       raw = JSON.stringify(defaults);
       localStorage.setItem(LS_KEYS, raw);
